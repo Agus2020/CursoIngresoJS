c5316e5556b9e1953d6340dc529b6455050cfed8
Agus2020 <60743500+Agus2020@users.noreply.github.com>
2020-02-07 12:27:58 -0300

    Ingresamos precio y descuento

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,12 @@
 function mostrar()
 {
-
+	var precio;
+	var descuento;
+	var total;
+	precio=prompt("Ingrese el precio");
+	precio=parseInt(precio);
+	descuento=prompt("Ingrese descuento");
+	descuento=parseInt(descuento);
+	total=precio/descuento+(precio);
+	document.getElementById('elPrecioFinal').value=total;
 }
