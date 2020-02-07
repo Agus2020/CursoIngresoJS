7a17a286774e5a162a2192e60720e6874c45c95b
Agus2020 <60743500+Agus2020@users.noreply.github.com>
2020-02-07 10:40:53 -0300

    Aprendemos a hacer una calculadora que toma por id

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,60 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var primerNumero;
+	var segundoNumero;
+	var suma;
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+	suma=primerNumero+segundoNumero;
+	alert(suma);
+
 }
 
 function restar()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resta;
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+	if(primerNumero>segundoNumero)
+		resta=primerNumero-segundoNumero;
+	if(segundoNumero>primerNumero)
+		resta=segundoNumero-primerNumero;
+	alert(resta);
 }
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+	var segundoNumero;
+	var multiplicar;
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+	multiplicar=primerNumero*segundoNumero;
+	alert(multiplicar);
 }
 
 function dividir()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var division;
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+	if(primerNumero>segundoNumero)
+		division=primerNumero/segundoNumero;
+	if(segundoNumero>primerNumero)
+		division=segundoNumero/primerNumero;
+	alert(division);
 }
 
