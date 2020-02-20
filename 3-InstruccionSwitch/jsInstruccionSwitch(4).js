90614d22c96cae49ff1c75aa11ca2c2ff7ac0987
alumno <alumno>
2020-02-20 12:17:47 -0300

    Todo de Switch y hasta el 5 de while

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,7 +5,26 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+switch(mesDelAño)
+{
+	case 'Febrero':
+	alert('Tiene 28 dias');break;  // enero,marzo,mayo,julio,agosto,octubre,diciembre
+	case 'Enero':
+	case 'Marzo':
+	case 'Mayo':
+	case 'Julio':
+	case 'Agosto':
+	case 'Octubre':
+	case 'Diciembre':
+	alert('Tiene 31 dias');break;
+	case 'Abril':
+	case 'Junio':
+	case 'Septiembre':
+	case 'Noviembre':
+	alert("Este mes tiene 30 dias");
+	break;
+	default: alert('Mes no compatible');break
+}
 
 
 
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,6 +2,12 @@ function mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
+	while(numero>!0&&10>!numero)
+	{
+		alert('No entendes pelotudo?');
+		numero=prompt("ingrese un numero entre 0 y 10");
+	}
+	document.getElementById('Numero');
 
 
 }//FIN DE LA FUNCIÓN
