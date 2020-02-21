186ec621c58f2a3cd8de4c3813746d8bbf056e77
Agus2020 <60743500+Agus2020@users.noreply.github.com>
2020-02-22 00:50:34 +0100

    Terminado con do while

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,9 +4,10 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var num1;
+	var num2;
 	var respuesta='si';
-
+	
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -6,12 +6,29 @@ function mostrar()
 	
 	var respuesta='si';
 
-	while(respuesta!='no')
+	do
 	{
-		
-	
+		num=prompt('Ingrese un numero');
+		num=parseInt(num);
+		if(contador==0)
+		{
+			max=num;
+			min=num;
+		}
+		if(max<num)
+		{
+			max=num;
+		}
+		if(min>num)
+		{
+			min=num;
+		}
+		contador++;
+		respuesta=prompt('Desea continuar? si/no');
 	}
-
+	while(respuesta=='si');
+	document.getElementById('maximo').value=max;
+	document.getElementById('minimo').value=min;
 
 
 
