9fffae01f8e81c87bb6cf8d26ca493e1acd5f3f4
Agus2020 <60743500+Agus2020@users.noreply.github.com>
2020-02-22 00:36:06 +0100

    Terminamos while

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -2,6 +2,12 @@ function mostrar()
 {
 
 var clave = prompt("ingrese el número clave.");
-
+console.log(clave);
+while(clave!='utn750')
+{
+	alert('ayuda es "utn750"');
+	clave = prompt("ingrese el número clave.");
+}
+alert('Clave valida');
 
 }//FIN DE LA FUNCIÓN
