72cf440e872eb176e0089239c1af86709e90f076
agus2020 <aagustin067@gmail.com>
2020-02-23 04:35:46 -0300

    Ejercicio 7 terminado con y sin array

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,5 +1,53 @@
 function mostrar()
 {
+	var nota=new Array();
+	var total;
+	var sexo=new Array();
+	var contador;
+	var min;
+	var max=0;
+	var minsexo;
+	total=0;
+	for(i=0;i<5;i++)
+	{
+		nota[i]=prompt('Ingrese nota');
+		nota[i]=parseInt(nota[i]);
+		while(isNaN(nota[i]))
+		{
+			nota[i]=prompt('Ingrese una nota entre 1 y 10');
+			nota[i]=parseInt(nota[i]);
+		}
+		sexo[i]=prompt('Ingrese sexo');
+		while(sexo[i]!='f'&&sexo[i]!='m')
+		{
+			sexo[i]=prompt('Ingrese sexo.');
+		}
+		if(i==0)
+		{
+			min=nota[i];
+			minsexo=sexo[i];
+		}
+		else
+		{
+			if(min>nota[i])
+			{
+				min=nota[i];
+				minsexo=sexo[i];
+			}
+		}
+		if(nota[i]>5&&sexo[i]=='m')
+		{
+			max++;
+		}
+		total=total+nota[i];
+	}
+	alert('El promedio de las notas es '+total/i);
+	alert('La nota mas baja fue '+min+' y el sexo es '+minsexo);
+	alert('Cantidad de varones  '+max);
+}
+/*
+function mostrar()
+{
 	var nota;
 	var total;
 	var sexo;
@@ -25,26 +73,27 @@ function mostrar()
 		}
 		if(contador==0)
 		{
-			total=total+nota;
 			min=total;
 			minsexo=sexo;
 		}
 		else
 		{
 			total=total+nota;
-		}
-		if(min>nota)
-		{
-			min=nota;
-			minsexo=sexo;
+			if(min>nota)
+			{
+				min=nota;
+				minsexo=sexo;
+			}
 		}
 		if(nota>5&&sexo=='m')
 		{
 			max++;
 		}
+		total=total+nota;
 		contador++;
 	}
 	alert('El promedio de las notas es '+total/contador);
 	alert('La nota mas baja fue '+min+' y el sexo es '+minsexo);
 	alert('Cantidad de varones con nota 6 o mayor '+max);
 }
+*/
